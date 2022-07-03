01964386139ff450363eeb6de6f761c77e7e69b5
Kirtee <kirtee.praja@gmail.com>
2022-07-03 23:26:18 +0530

    Tik Tik app Backend Set

---
+++ components/Sidebar.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import GoogleLogin from 'react-google-login'
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai'
+import {ImCancelCircle} from 'react-icons/im'
+
+const Sidebar = () => {
+  const [showSideBar, setshowSideBar] = useState(true);
+  return (
+    <div>
+      <div
+        className="block xl:hidden m-2 ml-4 mt-3 text-xl"
+        onClick={() => setshowSideBar((prev) => !prev)}
+      >
+        {showSideBar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar
