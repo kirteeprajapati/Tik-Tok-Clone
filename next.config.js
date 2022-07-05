9811d2b73a26a6b0270e5689abefcbbe836b360c
Kirtee <kirtee.praja@gmail.com>
2022-07-05 22:09:09 +0530

    Updates in Projects

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains:['yt3.ggpht.com'],
-  }
-}
+    domains: ["lh3.googleusercontent.com"],
+  },
+};
 
 module.exports = nextConfig
