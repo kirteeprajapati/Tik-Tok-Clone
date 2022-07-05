0798b8c157a1409faed49fca347e18596a838304
Kirtee <kirtee.praja@gmail.com>
2022-07-05 19:05:47 +0530

    Updates in Projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
 import type { NextPage } from 'next'
 import axios from 'axios';
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
+interface IProps{
+  videos: Video[]
+}
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const Home = ({ videos }: IProps) => {
+  console.log(videos)
+  return (
+    <div className='flex flex-col gap-10 video h-full'>
+      {videos.length ? (
+        videos.map((video: Video) => (
+          <VideoCard post={video} key={video._id} />
+        ))
+      ) : (<NoResults text={'No Videos'} />)}
+    </div>
+  )
 }
+
 export const getServerSideProps=async () => {
-  const response = await axios.get();
-}
-export const getServerSideProps=async () => {
-  const response = await axios.get(`https://localhost:3000/api/post`);
+  const {data} = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data
+    }
+  }
 }
 export default Home
