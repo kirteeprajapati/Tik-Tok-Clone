deeda3c77790d663a4e976ecd5384ab90ffd58be
Kirtee <kirtee.praja@gmail.com>
2022-07-06 21:26:32 +0530

    Updates in Projects

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,41 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { GoogleLogin, GoogleLogout } from 'react-google-login'
-import { AiOutlineLogout } from 'react-icons/ai';
-import { BiSearch } from 'react-icons/bi';
-import { IoMdAdd } from 'react-icons/io';
-import logo from '../utils/tiktik-logo.png'
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import logo from "../utils/tiktik-logo.png";
+import { createOrGetUser } from "../utils";
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-        <div className='w-[100px] md:w-[130px]'>
-          <Image className='cursor-pointer' src={logo} alt="TikTik" Layout= "responsive"/>
+        <div className="w-[100px] md:w-[130px]">
+          <Image
+            className="cursor-pointer"
+            src={logo}
+            alt="TikTik"
+            Layout="responsive"
+          />
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response)}
+            onError={() => console.log("Error")}
+          />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,17 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { allPostsQuery } from '../../../utils/queries'
-import { client } from '../../../utils/clients'
-type Data = {
-    name: string
-}
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse) {
+import { allPostsQuery } from '../../../utils/queries';
+import { client } from '../../../utils/client';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         const query = allPostsQuery();
+
         const data = await client.fetch(query);
+
         res.status(200).json(data);
+    } else if (req.method === 'POST') {
+        const doc = req.body;
+
+        client.create(doc).then(() => {
+            res.status(200).json('video created');
+        });
     }
 }
